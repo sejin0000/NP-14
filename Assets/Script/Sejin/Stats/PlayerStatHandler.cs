5b8a4c61ac4f7ddcc15ac752dbf19239f87a4aae
sejin0000 <141595906+sejin0000@users.noreply.github.com>
2023-11-02 18:15:30 +0900

    [feat] 플레이어 무기 시각 효과 적용
    + 플레이어 마우스 따라가는 총구

--- Assets/Script/Sejin/Stats/PlayerStatHandler.cs
+++ Assets/Script/Sejin/Stats/PlayerStatHandler.cs
@@ -22,10 +22,12 @@ public class PlayerStatHandler : MonoBehaviour
     [HideInInspector] public SpriteLibraryAsset PlayerSprite; // 스프라이트
     [HideInInspector] public SpriteLibraryAsset WeaponSprite; // 스프라이트
 
-    [HideInInspector] public float curHP { get { return curHP;  } set { if (value > HP.total) curHP = HP.total;  } } //현재 체력
-    [HideInInspector] public bool CanReload;            //장전   가능한지
-    [HideInInspector] public bool CanSkill;             //스킬   가능한지
-    [HideInInspector] public bool CanRoll;              //구르기 가능한지
+    [HideInInspector] public float CurHP   { get { return CurHP;  } set { if (value > HP.total) CurHP = HP.total;  } }               //현재   체력
+    [HideInInspector] public float CurAmmo { get { return CurAmmo;  } set { if (value > AmmoMax.total) CurAmmo = AmmoMax.total;  } } //현재   잔탄
+    [HideInInspector] public bool  CanReload;                              //장전   가능한지
+    [HideInInspector] public bool  CanSkill;                               //스킬   가능한지
+    [HideInInspector] public bool  CanRoll;                                //구르기 가능한지
+    [HideInInspector] public bool  Invincibility;                          //무적
 
     private void Awake()
     {
@@ -41,10 +43,11 @@ public class PlayerStatHandler : MonoBehaviour
         AmmoMax        =  new Stats(playerStats.ammoMax);
         PlayerSprite   =  playerStats.playerSprite;
         WeaponSprite   =  playerStats.weaponSprite;
-        curHP          =  HP.total;
+        CurHP          =  HP.total;
         CanReload      =  true;
         CanSkill       =  true;
         CanRoll        =  true;
+        Invincibility  =  false;
     }
 
 }
