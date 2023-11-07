f5a368d87a564638d79fe03d58960eb8d41ea2f9
sejin0000 <141595906+sejin0000@users.noreply.github.com>
2023-11-07 14:46:46 +0900

    [feat] 스킬 기본 구성

---
+++ Assets/Script/Skill/Skill.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    public int curSkillCool;
+
+
+    private void Update()
+    {
+        
+    }
+
+
+    private void SkillStart()
+    {
+        
+    }
+
+
+
+    private void SkillEnd()
+    {
+
+    }
+
+
+
+
+
+
+
+}
