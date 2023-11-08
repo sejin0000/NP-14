9fe6219ba3e232450873c201bd673641f55d0ea8
sejin0000 <141595906+sejin0000@users.noreply.github.com>
2023-11-08 15:46:44 +0900

    [Fix] 총알 발사 이벤트 수정

--- Assets/Script/Sejin/Stats/Stats.cs
+++ Assets/Script/Sejin/Stats/Stats.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Stats
 {
     public float total       { get { return (basic + added) * coefficient; } }   //  ÃÑ ½ºÅÈ °ª
