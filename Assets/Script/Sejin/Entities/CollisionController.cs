5622238b83f7985878fd6d0274d5decdd24a1f9c
sejin0000 <141595906+sejin0000@users.noreply.github.com>
2023-11-08 17:34:12 +0900

    [feat] 맞는 부분에 무적 조건 추가

--- Assets/Script/Sejin/Entities/CollisionController.cs
+++ Assets/Script/Sejin/Entities/CollisionController.cs
@@ -14,7 +14,7 @@ public class CollisionController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("EnemyBullet"))
+        if(collision.gameObject.layer == LayerMask.NameToLayer("EnemyBullet") && !playerStat.Invincibility)
         {
             float damage = collision.gameObject.GetComponent<Bullet>().ATK;
             playerStat.Damage(damage);
