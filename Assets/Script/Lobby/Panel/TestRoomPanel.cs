d5857bdb72d3d91418fef53d78c43cf34d4b6022
francJi <minkyuji95@hanmail.net>
2023-11-09 17:49:47 +0900

    feat : 테스트 룸 패널 추가 (3) [지민규]
    인스펙터에서 연결 및 테스트룸 유니티에서 구성 완료

--- Assets/Script/Lobby/Panel/TestRoomPanel.cs
+++ Assets/Script/Lobby/Panel/TestRoomPanel.cs
@@ -1,18 +1,76 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestRoomPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Buttons")]
+    public Button TestStartButton;
+    public Button BackButton;
+    public Button OpenOptionButton;
+
+    [Header("SceneStatus")]
+    public TextMeshProUGUI ConnectedSceneText;
+    [SerializeField] private string connectedScene;
+    public string ConnectedScene
     {
-        
+        get { return connectedScene; }
+        set
+        {
+            if (connectedScene != value)
+            {
+                connectedScene = value;
+                OnConnectedSceneChanged(connectedScene);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("RoomOptionPopup")]
+    public GameObject TestRoomOptionPopup;
+
+    [Header("LobbyPanel")]
+    public GameObject MainCanvas;
+    public LobbyPanel lobbyPanel;
+
+    private void Start()
     {
-        
+        Initialize();   
+    }
+    public void Initialize()
+    {
+        lobbyPanel = MainCanvas.GetComponent<LobbyPanel>();
+        TestStartButton.onClick.AddListener(OnTestStartButtonClickedInTest);
+        BackButton.onClick.AddListener(OnTestBackButtonClickedInTest);
+        OpenOptionButton.onClick.AddListener(OnOpenOptionButtonClicked);
+    }
+    public string OnConnectedSceneChanged(string connectedScene)
+    {
+        return connectedScene;
+    }
+
+    public void OnOpenOptionButtonClicked()
+    {
+        TestRoomOptionPopup.SetActive(true);
+    }
+
+    public void OnTestBackButtonClickedInTest()
+    {
+        if (lobbyPanel == null)
+        {
+            Initialize();
+        }
+        lobbyPanel.SetPanel(lobbyPanel.TestLobbyPanel.name);
+    }
+
+
+    public void OnTestStartButtonClickedInTest()
+    {
+        PhotonNetwork.CurrentRoom.IsOpen = false;
+        PhotonNetwork.CurrentRoom.IsVisible = false;
+
+        PhotonNetwork.LoadLevel(ConnectedScene);
     }
 }
