f7085eecaab7f5a30ae3a2d9b78ac5c2a77e49cb
sejin0000 <141595906+sejin0000@users.noreply.github.com>
2023-11-13 16:54:27 +0900

    [Fix] 플레이어 발사 시스템 수정

--- Assets/Script/Weapon/WeaponSystem.cs
+++ Assets/Script/Weapon/WeaponSystem.cs
@@ -32,20 +32,20 @@ public class WeaponSystem : MonoBehaviour
             Quaternion rot = muzzleOfAGun.transform.rotation;
             rot.eulerAngles += new Vector3(0, 0, Random.Range(-1 * _controller.playerStatHandler.BulletSpread.total, _controller.playerStatHandler.BulletSpread.total));// ¡ﬂø‰«‘
 
-            pv.RPC("BS", RpcTarget.All, rot, _controller.playerStatHandler.ATK.total, _controller.playerStatHandler.BulletLifeTime.total);
+            pv.RPC("BS", RpcTarget.All, rot, _controller.playerStatHandler.ATK.total, _controller.playerStatHandler.BulletLifeTime.total,(int)target, isDamage);
         }
     }
 
     [PunRPC]
-    public void BS(Quaternion rot, float Atk, float bulletLifeTime)//BulletSpawn
+    public void BS(Quaternion rot, float Atk, float bulletLifeTime,int _target, bool _isDamage)//BulletSpawn
     {
         GameObject _object =  Instantiate(bullet, muzzleOfAGun.transform.position, rot);
         Bullet _bullet = _object.GetComponent<Bullet>();
 
         _bullet.ATK = Atk;
         _bullet.BulletLifeTime = bulletLifeTime;
-        _bullet.target = target;
-        _bullet.IsDamage = isDamage;
+        _bullet.target = (BulletTarget)_target;
+        _bullet.IsDamage = _isDamage;
         _object.GetComponent<SpriteRenderer>().sprite = _controller.playerStatHandler.BulletSprite;
     }
 }
