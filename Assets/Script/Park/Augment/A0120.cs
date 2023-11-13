3b494d8863913554891cb5a81d63b354b1b6d711
plumas90 <plumas90@naver.com>
2023-11-13 18:15:28 +0900

    [fix]임시저장

--- Assets/Script/Park/Augment/A0120.cs
+++ Assets/Script/Park/Augment/A0120.cs
@@ -1,18 +1,36 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class A0120 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+public class A0120 : MonoBehaviourPun
+{
+        private TopDownCharacterController controller;
+        private PlayerStatHandler playerStat;
+        private void Awake()
+        {
+            if (photonView.IsMine)
+            {
+                controller = GetComponent<TopDownCharacterController>();
+                playerStat = GetComponent<PlayerStatHandler>();
+                controller.OnSkillEvent += aa; // 중요한부분
+            }
+        }
+        // Update is called once per frame
+        void aa()
+        {
+            playerStat.HP.added += 0.01f;
+        }
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (photonView.IsMine) 
+        {
+            if (collision.gameObject.tag == "Enemy") 
+            {
+                GameObject target = collision.gameObject;
+                EnemyAI a = target.GetComponent<EnemyAI>();
+            }
+        }
     }
 }
