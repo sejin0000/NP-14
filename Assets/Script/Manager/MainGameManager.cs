f67c55f5f5330716698f4fd875cf64de7689c6ee
francJi <minkyuji95@hanmail.net>
2023-11-15 11:40:12 +0900

    fix : spawnPoint 오브젝트 파괴 [지민규]

--- Assets/Script/Manager/MainGameManager.cs
+++ Assets/Script/Manager/MainGameManager.cs
@@ -375,6 +375,7 @@ public class MainGameManager : MonoBehaviourPunCallbacks
                         Debug.Log(currentMonsterCount);
                         EnemySpawn enemySpawn = go.GetComponent<EnemySpawn>();
                         enemySpawn.Spawn();
+                        Destroy(go);
                     }
                 }
             }
