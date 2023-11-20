7de41f060bb1767dc3127d113efa82941d225cc9
sejin0000 <141595906+sejin0000@users.noreply.github.com>
2023-11-20 14:55:26 +0900

    [Fix]간단 수정

--- Assets/Script/Sejin/Map/TestRoom.cs
+++ Assets/Script/Sejin/Map/TestRoom.cs
@@ -11,8 +11,9 @@ public class TestRoom : MonoBehaviour
 
     public void ChooseRoom()
     {
-        MapGenerator mapGenerator = GetComponent<MapGenerator>();
-        startPos.transform.position = (Vector2)mapGenerator.lastRoomList[0].center - (mapGenerator.mapSize / 2);
-        endPos.transform.position = (Vector2)mapGenerator.lastRoomList[mapGenerator.lastRoomList.Count - 1].center - (mapGenerator.mapSize / 2);
+
+        //MapGenerator mapGenerator = GetComponent<MapGenerator>();
+        //startPos.transform.position = (Vector2)mapGenerator.lastRoomList[0].center - (mapGenerator.mapSize / 2);
+        //endPos.transform.position = (Vector2)mapGenerator.lastRoomList[mapGenerator.lastRoomList.Count - 1].center - (mapGenerator.mapSize / 2);
     }
 }
