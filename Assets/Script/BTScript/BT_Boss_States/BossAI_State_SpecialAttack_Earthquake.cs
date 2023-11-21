312975393be7339a6ca3d606efb1e87136250ab4
Woomingyu <119576417+Woomingyu@users.noreply.github.com>
2023-11-21 23:27:23 +0900

    중간 세이브

---
+++ Assets/Script/BTScript/BT_Boss_States/BossAI_State_SpecialAttack_Earthquake.cs
@@ -0,0 +1,19 @@
+using myBehaviourTree;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI_State_SpecialAttack_Earthquake : BTAction
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
