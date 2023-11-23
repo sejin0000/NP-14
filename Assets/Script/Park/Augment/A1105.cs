56800d269db04aab36ea67a0e1b391e892fc0f99
francJi <minkyuji95@hanmail.net>
2023-11-23 20:50:50 +0900

    fix : A1105 수정
    - WeaponSystem의 BulletTarget이 상황에 맞게 작용하도록 조건식 추가

--- Assets/Script/Park/Augment/A1105.cs
+++ Assets/Script/Park/Augment/A1105.cs
@@ -38,6 +38,29 @@ public class A1105 : MonoBehaviourPun
         while (true)
         {
             WeaponSystem.isDamage = !WeaponSystem.isDamage;
+            var targets = WeaponSystem.targets;
+            if (WeaponSystem.isDamage) 
+            {
+                if (targets.ContainsKey("Enemy"))
+                    continue;
+                else
+                {
+                    targets["Enemy"] = (int)BulletTarget.Enemy;
+                    targets.Remove("Player");
+                    Debug.Log("A1105 - 공격모드 실행");
+                }
+            }
+            else
+            {
+                if (targets.ContainsKey("Player"))
+                    continue;
+                else
+                {
+                    targets["Player"] = (int)BulletTarget.Player;
+                    targets.Remove("Enemy");
+                    Debug.Log("A1105 - 힐모드 실행");
+                }
+            }
             yield return autoTime;
         }
     }
