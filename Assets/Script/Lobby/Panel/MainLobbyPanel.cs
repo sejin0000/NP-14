29b499c74745468d00c1ddccf2655daa3956648f
francJi <minkyuji95@hanmail.net>
2023-11-24 20:39:25 +0900

    feat : MainLobbyPanel (2)
    - PunCallbacks 추가

--- Assets/Script/Lobby/Panel/MainLobbyPanel.cs
+++ Assets/Script/Lobby/Panel/MainLobbyPanel.cs
@@ -1,9 +1,13 @@
 using Photon.Pun;
+using Photon.Realtime;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class MainLobbyPanel : MonoBehaviourPunCallbacks
 {
@@ -37,11 +41,11 @@ public class MainLobbyPanel : MonoBehaviourPunCallbacks
 
         // DESC : connect buttons
         characterSelectButton.onClick.AddListener(playerInfo.OnCharacterButtonClicked);
-        testLobbyButton.onClick.AddListener(LobbyManager.Instance.OnTestLobbyButtonClicked);
-        gameRoomButton.onClick.AddListener(LobbyManager.Instance.OnGameRoomButtonClicked);
+        testLobbyButton.onClick.AddListener(OnTestLobbyButtonClicked);
+        gameRoomButton.onClick.AddListener(OnGameRoomButtonClicked);
 
         // DESC : instantiate Player
-        InstantiatePlayer();
+        //InstantiatePlayer();
 
         // DESC : 커스텀 프로퍼티 - Char_Class 추가
         LobbyManager.Instance.ClassNum = playerInfo.GetCharClass();
@@ -51,4 +55,63 @@ public class MainLobbyPanel : MonoBehaviourPunCallbacks
     {
         LobbyManager.Instance.instantiatedPlayer = Instantiate(Resources.Load<GameObject>(PrefabPathes.PLAYER_INLOBBY_PREFAB_PATH));
     }
+
+    #region MonoBehaviorPunCallbacks
+    public override void OnJoinedLobby()
+    {
+        Debug.Log($"MainLobbyPanel - Current State : {Enum.GetName(typeof(PanelType), LobbyManager.Instance.CurrentState)}");
+        if (LobbyManager.Instance.CurrentState != PanelType.MainLobbyPanel)
+        {
+            return;
+        }
+    }
+
+    // DESC : 랜덤룸 진입 (OnGameButton) 에 실패했을 경우, 방을 만듦.
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        if (LobbyManager.Instance.CurrentState != PanelType.MainLobbyPanel)
+        {
+            return;
+        }
+
+        // DESC : 룸 생성
+        string roomName = $"RandRoom{Random.Range(1, 200)}";
+        RoomOptions options = new RoomOptions { MaxPlayers = 3 };     
+        options.CustomRoomProperties = new Hashtable() { { CustomProperyDefined.TEST_OR_NOT, false } };
+        PhotonNetwork.CreateRoom(roomName, options, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (LobbyManager.Instance.CurrentState != PanelType.MainLobbyPanel)
+        {
+            return;
+        }
+
+        LobbyManager.Instance.SetPanel(PanelType.RoomPanel);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        if (LobbyManager.Instance.CurrentState != PanelType.MainLobbyPanel)
+        {
+            return;
+        }
+
+        LobbyManager.Instance.SetPanel(PanelType.RoomPanel);
+    }
+    #endregion
+
+    #region Buttons
+    private void OnGameRoomButtonClicked()
+    {
+        var CustomRoomProperties = new Hashtable() { { CustomProperyDefined.TEST_OR_NOT, false } };
+        PhotonNetwork.JoinRandomRoom(CustomRoomProperties, 0);        
+    }
+
+    public void OnTestLobbyButtonClicked()
+    {
+        LobbyManager.Instance.SetPanel(PanelType.TestLobbyPanel);
+    }
+    #endregion
 }
