593346e455e898cc62606d6e0fe973ddd69055df
francJi <minkyuji95@hanmail.net>
2023-11-24 20:39:53 +0900

    feat : 커스텀 프로퍼티 관리

---
+++ Assets/Script/Define/CustomProperyDefined.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomProperyDefined
+{
+    #region PlayerCustomProperties
+    public static readonly string CLASS_PROPERTY = "Char_Class";
+    public static readonly string ASK_READY_PROPERTY = "IsPlayerReady";
+    #endregion
+
+    #region RoomCustomProperties
+    public static readonly string TEST_OR_NOT = "IsTest";
+    #endregion
+}
