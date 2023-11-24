0c4979cb8889af3df96daef9c7de00bb348d0645
francJi <minkyuji95@hanmail.net>
2023-11-24 16:52:42 +0900

    feat : Path 설정 스크립트 추가

---
+++ Assets/Script/Define/PrefabPathes.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabPathes
+{
+    #region Player
+    public static readonly string PLAYER_PREFAB_PATH = "/pefabs/Player";
+    #endregion
+}
