29bb8c92de7ad1762b9b154d00ba50cdbd3c2cb8
plumas90 <plumas90@naver.com>
2023-11-30 11:50:52 +0900

    [fix]슬롯 색,심볼 적용

--- Assets/Script/Park/AugmentControl/ChoiceSlot.cs
+++ Assets/Script/Park/AugmentControl/ChoiceSlot.cs
@@ -16,7 +16,19 @@ public class ChoiceSlot : MonoBehaviour//눌러서 골르는 증강 슬롯 열렸을때 해당증
     public int listIndex;
     public ResultManager Parent;
     int rare;
+    int symbolNum;
 
+    public Sprite tier1;
+    public Sprite tier2;
+    public Sprite tier3;
+
+    public Sprite symbolAll;
+    public Sprite symbolSniper;
+    public Sprite symbolShotgun;
+    public Sprite symbolsoldier;
+
+    public Image bodyImage;
+    public Image symbolImage;
     //[Range(1, 3)] int StatType;
 
     private void OnEnable()// 이름,정보,색 업데이트
@@ -25,20 +37,39 @@ public class ChoiceSlot : MonoBehaviour//눌러서 골르는 증강 슬롯 열렸을때 해당증
         Ispick = false;
         Info.text = stat.func;
         rare = stat.Rare;
-        Image image = gameObject.GetComponent<Image>();
-        //Debug.Log($"{rare}");
-        switch (rare)//증강 티어에 따라 색넣어주는데 색 뭔가 이상함 나중에 머터리얼 넣어줘야할듯
+        symbolNum = stat.Code % 1000;
+        Debug.Log($"심볼 넘버 {symbolNum}");
+        bodyImage = gameObject.GetComponent<Image>();
+        
+        switch (rare)
         {
             case 1:
-                image.color = new Color(205 / 255f, 127 / 255f, 50 / 255f);//브 
+                bodyImage.sprite = tier1;
                 break;
 
             case 2:
-                image.color = new Color(192 / 255f, 192 / 255f, 192 / 255f);//실
+                bodyImage.sprite = tier2;
                 break;
 
             case 3:
-                image.color = new Color(255 / 255f, 215 / 255f, 0 / 255f);//골
+                bodyImage.sprite = tier3;
+                break;
+        }
+        switch (symbolNum)
+        {
+            case 0:
+                symbolImage.sprite = symbolAll;
+                break;
+            case 1:
+                symbolImage.sprite= symbolSniper;
+                break;
+
+            case 2:
+                symbolImage.sprite = symbolsoldier;
+                break;
+
+            case 3:
+                symbolImage.sprite = symbolShotgun;
                 break;
         }
     }
