0e623d49e4cbb3aa80a1c71232abc1dbbfcbedbe
sejin0000 <141595906+sejin0000@users.noreply.github.com>
2023-12-12 19:58:01 +0900

    [feat] 조작키 패널

---
+++ Assets/KeySetting.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySetting : MonoBehaviour
+{
+    public void KeySettingPanelClose()
+    {
+        gameObject.SetActive(false);
+    }
+}
