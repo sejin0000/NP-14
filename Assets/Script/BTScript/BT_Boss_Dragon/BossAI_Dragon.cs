80f654eb9ac03a698e2a6b07e631ab57b969a4a4
francJi <minkyuji95@hanmail.net>
2023-12-15 00:49:39 +0900

    Update BossAI_Dragon.cs

--- Assets/Script/BTScript/BT_Boss_Dragon/BossAI_Dragon.cs
+++ Assets/Script/BTScript/BT_Boss_Dragon/BossAI_Dragon.cs
@@ -406,6 +406,9 @@ public class BossAI_Dragon : MonoBehaviourPunCallbacks, IPunObservable
 
     public void UpdateBreath()
     {
+        if (!PhotonNetwork.IsMasterClient)
+            return;
+
         currentTime -= Time.deltaTime;
 
         if (currentTime <= 0)
@@ -599,6 +602,9 @@ public class BossAI_Dragon : MonoBehaviourPunCallbacks, IPunObservable
     //진짜 진짜 공격&넉백임
     public void AttackTargetsInArea(int areaIndex, float attackCoefficient = 1f)
     {
+        if (!PhotonNetwork.IsMasterClient)
+            return;
+
         if (inToAreaPlayers == null || areaIndex < 0 || areaIndex > AreaList.Length)
             return;
 
